32b32641f17a50e03fa9aff3fe479699f91ceab5
Keith Clark <keith@keithclark.co.uk>
2020-06-27 18:47:49 +0100

    Tidy pretty print

--- tools/convert/lib/prettyPrint.js
+++ tools/convert/lib/prettyPrint.js
@@ -1,5 +1,9 @@
-
-
+/**
+ * Applys "pretty print" to a song
+ *
+ * @param {*} src
+ * @param {*} instrumentNames
+ */
 
 export const prettyPrint = (src, instrumentNames = []) => {
   let stack = [];
@@ -11,56 +15,61 @@ export const prettyPrint = (src, instrumentNames = []) => {
     const rootIndex = stack[0] || 0
     const maxDepth = maxDepths[rootIndex]
 
+    // Opening array or object requires an indent until we hit the maxDepth for
+    // the current root index:
     if (chr === '[' || chr === '{') {
       index = 0;
       stack.push(index);
       depth = stack.length - 1;
+      const parentIndex = stack[depth - 1];
 
       // If this is an instrument, add a comment to ease editing
       if (rootIndex === 0 && depth === 2) {
-        chr = `/* ${instrumentNames[stack[depth-1]].substr(0,22).padEnd(24,' ')} */ ` + chr;
+        chr = `/* ${instrumentNames[parentIndex].substr(0,22).padEnd(24,' ')} */ ${chr}`;
       }
 
       // If this is a pattern add a comment
       else if (rootIndex === 1 && depth === 2) {
-        chr = `/* Pattern ${stack[depth-1]} */ ` + chr;
+        chr = `/* Pattern ${parentIndex} */ ${chr}`;
       }
 
       // If this is a channel, add a comment
       else if (rootIndex === 1 && depth === 3) {
-        chr = `/* Channel ${stack[depth-1]} */ ` + chr;
+        chr = `/* Channel ${parentIndex} */ ${chr}`;
       }
 
       if (depth > maxDepth) {
-        return chr
+        return chr;
       }
       const indent = '  '.repeat(depth + 1);
       return chr + '\n' + indent;
     }
 
+    // Outdent when closing and array or object
     if (chr === ']' || chr === '}') {
       index = stack.pop();
       depth = stack.length - 1;
 
       if (depth >= maxDepth) {
-        return chr
+        return chr;
       }
       const indent = '  '.repeat(depth + 1);
       return '\n' + indent + chr
     }
 
+    // Maintain depth for elements
     if (chr === ',') {
       depth = stack.length - 1;
       index = stack[depth];
       stack[depth]++;
 
       if (depth > maxDepth) {
-        return chr
+        return chr;
       }
       const indent = '  '.repeat(depth + 1);
       return chr + '\n' + indent
     }
     return chr;
-  }).join('')
-  return src.replace(/(\[[^\[]+?)/,'\n$1')
-}
+  }).join('');
+
+};
