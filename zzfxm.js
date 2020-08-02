eb8c5d4af02ab6229bd4574f941253c942ecd7ce
Keith Clark <keith@keithclark.co.uk>
2020-08-02 12:19:45 +0100

    Implemented format change. Updated renderer and songs.

--- zzfxm.js
+++ zzfxm.js
@@ -1,14 +1,13 @@
 /**
- * ZzFX Music Generator v1.0.0 by Keith Clark
+ * ZzFX Music Renderer v2.0.0 by Keith Clark
  */
-
 
 /**
  * @typedef Channel
- * @type {Array.<Number, Number, Number>}
- * @property {Number} 0 - Note period (1-36)
- * @property {Number} 1 - Instrument (1-16)
- * @property {Number} 2 - Attenuation (1-64)
+ * @type {Array.<Number>}
+ * @property {Number} 0 - Channel instrument
+ * @property {Number} 1 - Channel panning (-1 to +1)
+ * @property {Number} 2 - Note
  */
 
 /**
@@ -25,134 +24,99 @@
  * Generate a song
  *
  * @param {Array.<Instrument>} instruments - Array of ZzFX sound paramaters.
- * @param {Array.<Pattern>} patterns - Array of `pattern` data.
- * @param {Array.<Number>} sequence - Array of `pattern` indexes.
- * @param {Number} [speed=6] - Playback speed of the song
- * @param {Array.<Number>} panning - Array of `panning` values for each channel
- * @returns {Array.<Array.<Number>>} Sample data for the song
- *
- * @example
- * const song = zzfxM(
- *   [                                         // Instruments
- *     [,0,219,,,,,1.1,,-.1,-50,-.05,-.01,1],    // #1 - Snare
- *     [2,0,84,,,.1,,.7,,,,.5,,6.7,1,.05]        // #2 - Kick drum
- *   ],
- *   [                                         // Patterns
- *     [               // Pattern 0
- *       [               // Channel 0
- *         1, 1, 0,        // Using instrument 1, play C-1
- *         0, 0, 0,
- *         1, 5, 32,       // Using instrument 1, play E-1 with 50% attenuation
- *         0, 0, 0
- *       ],
- *       [               // Channel 1
- *         2, 8, 0,        // Using instrument 2, play G-1
- *         2, 8, 16,       // Using instrument 2, play G-1 with 25% attenuation
- *         1, 3, 0,        // Using instrument 3, play D-1
- *         0, 0, 0
- *       ]
- *     ]
- *   ],
- *   [                                         // Sequence
- *     0, 0, 0, 0                                // Play pattern 0 four times
- *   ],
- *   4,                                        // Playback speed 4
- *   [
- *     -1, 1                                   // Panning info. ch1=left ch2=right
- *   ]
- * ]
- * );
- * zzfxP(song);                                // Play the song
+ * @param {Array.<Pattern>} patterns - Array of pattern data.
+ * @param {Array.<Number>} sequence - Array of pattern indexes.
+ * @param {Number} [speed=125] - Playback speed of the song (in BPM).
+ * @returns {Array.<Array.<Number>>} Left and right channel sample data.
  */
-const zzfxM = (instruments, patterns, sequence, speed = 6, panning) => {
-  let SAMPLE_RATE = 44100;                  // this must match ZzFX sample rate
-  let BPM = 125                             // beats per min - always 125
-  let ticksPerSecond = 1000 / (2500 / BPM);
-  let tickRowSize = SAMPLE_RATE / (ticksPerSecond / speed) | 0;
-  let bufferStart = 0
-  let bufferPos;
-  let bufferOffset;
+
+const zzfxM = (instruments, patterns, sequence, BPM = 125) => {
+  let instrumentParameters;
+  let i;
+  let j;
+  let k;
+  let note;
   let sample;
-  let sampleCache = {};
-  let sampleCacheKey;
-  let sampleByte;
-  let sampleOffset;
-  let noteIndex;
-  let pan;
-  let attenuation;
+  let patternChannel;
+  let notFirstBeat;
+  let stop;
   let instrument;
-  let instrumentParams;
-  let period;
-  let leftChannelBuffer=[];
-  let rightChannelBuffer=[];
+  let pitch;
+  let attenuation;
+  let outSampleOffset;
+  let sampleOffset;
+  let nextSampleOffset;
+  let sampleBuffer = [];
+  let leftChannelBuffer = [];
+  let rightChannelBuffer = [];
+  let channelIndex = 0;
+  let hasMore = 1;
+  let sampleCache = {};
+  let beatLength = zzfxR / BPM * 60 >> 2;
 
-  sequence.map(patternIndex => {
+  // for each channel in order until there are no more
+  for(; hasMore; channelIndex++) {
 
-    // Walk over each channel of the pattern.
-    patterns[patternIndex].map((channel, channelIndex) => {
+    // reset current values
+    sampleBuffer = [hasMore = notFirstBeat = pitch = outSampleOffset = 0];
 
-      // for each channel we need to reset the buffer offset so we can layer
-      // sample data.
-      bufferOffset = bufferStart;
+    // for each pattern in sequence
+    sequence.map((patternIndex, sequenceIndex) => {
+      // get pattern for current channel, use empty 1 note pattern if none found
+      patternChannel = patterns[patternIndex][channelIndex] || [0, 0, 0];
 
-      // Determin the panning value for this channel. If no panning was provided
-      // we fallback to alternating each song channel between the left and right
-      // speakers (even channel indexes will come from the left speaker, odd
-      // from the right speaker)
-      pan = panning ? .5 + (panning[channelIndex] || 0) / 2 : channelIndex & 1;
+      // check if there are more channels
+      hasMore |= !!patterns[patternIndex][channelIndex];
 
-      // Read the channel data.
-      for (noteIndex = 0; noteIndex < channel.length; bufferOffset += tickRowSize) {
+      // get next offset, use the length of first channel
+      nextSampleOffset = outSampleOffset + (patterns[patternIndex][0].length - 2 - !notFirstBeat) * beatLength;
 
-        // If there's a new instrument, set it.
-        instrument = channel[noteIndex++] || instrument;
+      // for each beat in pattern, plus one extra if end of sequence
+      for (i = 2, k = outSampleOffset; i < patternChannel.length + (sequenceIndex == sequence.length - 1); notFirstBeat = ++i) {
 
-        // If we have a period then we need to play a note using the current
-        // instrument.
-        period = channel[noteIndex++];
-        if (period) {
-          // Reset the attenuation and sample buffer offset so we can play a new
-          // note.
-          attenuation = sampleOffset = 0;
+        // <channel-note>
+        note = patternChannel[i];
 
-          // Create a unique key for this sample (this will coerced to a string)
-          sampleCacheKey = [period,instrument];
+        // stop if different instrument or new note
+        stop = instrument != patternChannel[0] | note | 0;
 
-          // If we haven't done so already, build and cache the ZzFX sample for
-          // the current period and instrument. Without caching the browser will
-          // will hang.
-          if (!sampleCache[sampleCacheKey]) {
-            instrumentParams = [...instruments[instrument - 1]];
-            instrumentParams[2] *= 2 ** ((period - 12) / 12);
-            sampleCache[sampleCacheKey] = zzfxG(...instrumentParams);
-          }
+        // fill buffer with samples for previous beat, most cpu intensive part
+        for (j = 0; j < beatLength && notFirstBeat;
 
-          sample = sampleCache[sampleCacheKey];
+            // fade off attenuation at end of beat if stopping note, prevents clicking
+            j++ > beatLength - 99 && stop ? attenuation += (attenuation < 1) / 99 : 0
+        ) {
+          // copy sample to stereo buffers with panning
+          sample = (1 - attenuation) * sampleBuffer[sampleOffset++] / 2 || 0;
+          leftChannelBuffer[k] = (leftChannelBuffer[k] || 0) + sample * patternChannel[1] - sample;
+          rightChannelBuffer[k] = (rightChannelBuffer[k++] || 0) + sample * patternChannel[1] + sample;
         }
 
-        // Update the channel attenuation value. We'll use this to control the
-        // volume when rendering the sample slice for this row.
-        attenuation = (channel[noteIndex++] / 64) || attenuation;
-
-        // Fill the left and right audio buffers with data for this channel.
-        // If we have sample data (a new note or the remaining sample data from
-        // the previous row) then we attenuate it for the left channel using the
-        // channel panning value and again for the right channel. The values are
-        // then merged with the values already in the buffer. If the buffer is
-        // empty we need to prefill it with `0` values for periods of slience to
-        // prevent the clicking and popping noises caused by `undefined` values
-        // in some browsers.
-        for (bufferPos = bufferOffset; bufferPos < bufferOffset + tickRowSize; bufferPos++) {
-          sampleByte = sample && (sample[sampleOffset++] * (1 - attenuation)) || 0;
-          leftChannelBuffer[bufferPos] = (leftChannelBuffer[bufferPos] || 0) + sampleByte * (1 - pan);
-          rightChannelBuffer[bufferPos] = (rightChannelBuffer[bufferPos] || 0) + sampleByte * pan;
+        // set up for next note
+        if (note) {
+          // set attenuation
+          attenuation = note % 1;
+          if (note |= 0) {
+            // get cached sample
+            sampleBuffer = sampleCache[
+              [
+                instrument = patternChannel[sampleOffset = 0] || 0,
+                note
+              ]
+            ] = sampleCache[[instrument, note]] || (
+                // add sample to cache
+                instrumentParameters = [...instruments[instrument]],
+                instrumentParameters[2] *= 2 ** ((note - 12) / 12),
+                zzfxG(...instrumentParameters)
+            );
+          }
         }
       }
 
+      // update the sample offset
+      outSampleOffset = nextSampleOffset;
     });
-
-    bufferStart = bufferPos;
-  });
+  }
 
   return [leftChannelBuffer, rightChannelBuffer];
 }
