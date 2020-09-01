3fd6978d1529ea9ba7c9c9dcfaa6062149393775
Keith Clark <keith@keithclark.co.uk>
2020-09-01 20:21:17 +0100

    More CSS perf

--- tools/tracker/src/components/Channel.svelte
+++ tools/tracker/src/components/Channel.svelte
@@ -86,6 +86,7 @@ const handleScroll = event => {
   background-image: linear-gradient( #0001 18px, transparent 18px);
   background-size:100% 72px;
   white-space: pre-line;
+  will-change: transform;
 }
 .channel :global(.field) {
   align-items: center!important;
