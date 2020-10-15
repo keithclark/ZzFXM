2f24cd1015120d24bbc2199dee6c53961668b99b
Keith Clark <keith@keithclark.co.uk>
2020-10-15 23:18:55 +0100

    Support multiline in <TextProperty>

--- tools/tracker/src/components/TextProperty.svelte
+++ tools/tracker/src/components/TextProperty.svelte
@@ -4,8 +4,19 @@
   export let value = '';
   export let hint = '';
   export let size = 14;
+  export let multiline = false;
 </script>
 
 <Property {label} {hint} let:id={id}>
-  <input {id} style="width: {size+1}ch" spellcheck="false" type="text" class="input" bind:value={value}>
+  {#if multiline}
+    <textarea {id} style="width: {size+1}ch" rows="5" spellcheck="false" class="input" bind:value={value}></textarea>
+  {:else}
+    <input {id} style="width: {size+1}ch" spellcheck="false" type="text" class="input" bind:value={value}>
+  {/if}
 </Property>
+
+<style>
+  textarea {
+    resize: none;
+  }
+</style>
