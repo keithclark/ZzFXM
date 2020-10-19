804b6c1810164f9f61bba4b8eae9b789b3327bd2
Keith Clark <keith@keithclark.co.uk>
2020-10-20 00:43:29 +0100

    Safari CSS fixes

--- tools/tracker/src/components/About.svelte
+++ tools/tracker/src/components/About.svelte
@@ -14,19 +14,29 @@ div {
   text-align: center;
 }
 h1 {
-  background: linear-gradient(90deg, rgb(255,85,18), rgb(247,255,15) 25%, rgb(5,193,255) 50%, rgb(171,69,255) 75%, rgb(255,85,18));
-  background-size:2000px 2em;
-  background-repeat: repeat-x;
-  background-clip: text;
-  color: transparent;
-  filter: drop-shadow(1px 1px 0px #0004);
-  animation: 10s colorshock linear infinite;
-  font-size:250%;
+  font-size: 250%;
   letter-spacing: -0.06em;
+  color: rgb(255,85,18);
+  animation: 10s colorshock linear infinite;
+  filter: drop-shadow(1px 1px 0px #0004) hue-rotate(0);
 }
 @keyframes colorshock {
   from {
-    background-position:2000px 0;
+    filter: drop-shadow(1px 1px 0px #0004) hue-rotate(360deg);
+  }
+}
+@supports (background-clip: text) {
+  h1 {
+    background: linear-gradient(135deg, rgb(255,85,18), rgb(247,255,15) 25%, rgb(5,193,255) 50%, rgb(171,69,255) 75%, rgb(255,85,18));
+    background-size: 2000px 2em;
+    background-repeat: repeat-x;
+    background-clip: text;
+    color: transparent;
+  }
+  @keyframes colorshock {
+    from {
+      background-position: 2000px 0;
+    }
   }
 }
 </style>
